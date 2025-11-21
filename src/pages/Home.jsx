001342e93648d6e1a2f3de40e6ec40edbf114c9b
Ahmed-hesham95 <ahmedbob7898@gmail.com>
2025-11-21 11:25:24 +0200

    ProjectFinal456

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import Specialitymenu from '../components/Specialitymenu'
+import TopDoctors from '../components/TopDoctors'
+
+function Home() {
+  return (
+    <div>
+        <Header/>
+        <Specialitymenu/>
+        <TopDoctors/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/MyAppoinyments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyAppoinyments() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default MyAppoinyments
